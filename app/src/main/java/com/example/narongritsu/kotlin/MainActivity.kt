6559bee93244ef61ea75f41c78b67fcc6bf88bfc
Narongrit Sunatu <58160657@go.buu.ac.th>
2018-05-10 09:57:55 +0700

    kotlin

--- app/src/main/java/com/example/narongritsu/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/narongritsu/kotlin/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.narongritsu.kotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val user = "1234"
+    val pass = "5678"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textUser:EditText = findViewById<EditText>(R.id.username)
+//        val textUser:EditText = findViewById<EditText>(R.id.username)
+        username.text.toString()
+        password.text.toString()
+        button_login.setOnClickListener {
+            var chk = checkLogin()
+            Toast.makeText(this,""+chk,Toast.LENGTH_LONG).show()
+        }
     }
+
+    private fun checkLogin() : Boolean {
+        if (user == username.text.toString() && pass == password.text.toString()){
+            return true
+        }
+        return false
+    }
+
 }
