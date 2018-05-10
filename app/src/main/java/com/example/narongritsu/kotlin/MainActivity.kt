4f8fcbe7b87ace155e9bb6f37326fe991925602b
Narongrit Sunatu <58160657@go.buu.ac.th>
2018-05-10 10:22:59 +0700

    kotlin

--- app/src/main/java/com/example/narongritsu/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/narongritsu/kotlin/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.narongritsu.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     val user = "1234"
     val pass = "5678"
@@ -20,6 +24,9 @@ class MainActivity : AppCompatActivity() {
         button_login.setOnClickListener {
             var chk = checkLogin()
             Toast.makeText(this,""+chk,Toast.LENGTH_LONG).show()
+        }
+        for (i in 0..4){
+
         }
     }
 
