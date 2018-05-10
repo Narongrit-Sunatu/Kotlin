7a20b11aab46592eeeac20cc37de55818bd051b4
Narongrit Sunatu <58160657@go.buu.ac.th>
2018-05-10 09:32:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/narongritsu/kotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.narongritsu.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textUser:EditText = findViewById<EditText>(R.id.username)
+    }
+}
